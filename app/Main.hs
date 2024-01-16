6e08ed0a629942dd3daef8a79b628506637ca9f8
Junji Hashimoto <junji.hashimoto@gree.net>
2024-01-17 01:23:49 +0900

    Add show-detection-image command

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,12 @@ import Options.Applicative
 import qualified Data.Text as T
 import qualified Data.ByteString as BS
 import Data.FileEmbed (embedFile)
-import Data.OSC1337
-import System.FilePath
+import qualified Data.OSC1337 as OSC
+import qualified Data.Sixel as Sixel
+import System.FilePath ( (</>), takeBaseName, takeDirectory )
 import Codec.Picture
+import Text.Printf
+import System.Environment (lookupEnv)
 import Draw
 
 -- Add subcommands by optparse-applicative
@@ -24,9 +27,37 @@ data CocoCommand
   = ListImages { cocoFile :: FilePath }
   | ListCategories { cocoFile :: FilePath }
   | ListAnnotations { cocoFile :: FilePath }
+  | ListCocoResult { cocoResultFile :: FilePath }
   | ShowImage { cocoFile :: FilePath, imageFile :: FilePath, enableBoundingBox :: Bool }
+  | ShowDetectionImage
+  { cocoFile :: FilePath
+  , cocoResultFile :: FilePath
+  , imageFile :: FilePath
+  , scoreThreshold :: Maybe Double
+  }
   | BashCompletion
   deriving (Show, Eq)
+
+putImage :: Either FilePath (Image PixelRGB8) -> IO ()
+putImage image' = do
+  termProgram <- lookupEnv "TERM_PROGRAM"
+  image <- case image' of
+    Left imagePath -> do
+      imageBin <- readImage imagePath
+      case imageBin of
+        Left err -> fail $ "Image file " ++ imagePath ++ " can not be read."
+        Right imageBin -> return (convertRGB8 imageBin)
+    Right image -> return image
+  case termProgram of
+    Just "iTerm.app" -> do
+      OSC.putOSC image
+      putStrLn ""
+    Just "vscode" -> do
+      Sixel.putSixel image
+      putStrLn ""
+    _ -> do
+      Sixel.putSixel image
+      putStrLn ""     
 
 listImages :: Coco -> IO ()
 listImages coco = do
@@ -65,6 +96,52 @@ listAnnotations coco = do
   forM_ (cocoAnnotations coco) $ \CocoAnnotation {..} -> do
     putStrLn $ show cocoAnnotationId ++ "\t" ++ show cocoAnnotationImageId ++ "\t" ++ show cocoAnnotationCategory ++ "\t" ++ show cocoAnnotationSegment ++ "\t" ++ show cocoAnnotationArea ++ "\t" ++ show cocoAnnotationBbox ++ "\t" ++ show cocoAnnotationIsCrowd
 
+listCocoResult :: [CocoResult] -> IO ()
+listCocoResult cocoResults = do
+  putStrLn "-- list coco result --"
+  -- first column is image id
+  -- second column is category id
+  -- third column is score
+  -- fourth column is bbox
+  putStrLn "image_id\tcategory_id\tscore\tbbox"
+  forM_ cocoResults $ \cocoResult -> do
+    putStrLn $ show (cocoResultImageId cocoResult) ++ "\t" ++ show (cocoResultCategory cocoResult) ++ "\t" ++ show (cocoResultScore cocoResult) ++ "\t" ++ show (cocoResultBbox cocoResult)
+
+drawBoundingBox :: DynamicImage -> [CocoAnnotation] -> [CocoCategory] -> IO (Image PixelRGB8)
+drawBoundingBox imageBin annotations categories = do
+  let imageRGB8 = convertRGB8 imageBin
+  forM_ annotations $ \annotation -> do
+    let (CoCoBoundingBox (bx,by,bw,bh)) = cocoAnnotationBbox annotation
+        x = round bx
+        y = round by
+        width = round bw
+        height = round bh
+    drawRect x y (x+width) (y+height) (255,0,0) imageRGB8
+    drawString (T.unpack (cocoCategoryName (categories !! (cocoAnnotationCategory annotation - 1)))) x y (255,0,0) (0,0,0) imageRGB8
+  return imageRGB8
+
+drawDetectionBoundingBox :: DynamicImage -> [CocoResult] -> [CocoCategory] -> Maybe Double -> IO (Image PixelRGB8)
+drawDetectionBoundingBox imageBin annotations categories scoreThreshold = do
+  let imageRGB8 = convertRGB8 imageBin
+  forM_ annotations $ \annotation -> do
+    let (CoCoBoundingBox (bx,by,bw,bh)) = cocoResultBbox annotation
+        x = round bx
+        y = round by
+        width = round bw
+        height = round bh
+        draw = do
+          drawRect x y (x+width) (y+height) (255,0,0) imageRGB8
+          drawString (T.unpack (cocoCategoryName (categories !! (cocoResultCategory annotation - 1)))) x y (255,0,0) (0,0,0) imageRGB8
+          -- Use printf format to show score
+          drawString (printf "%.2f" (cocoResultScore annotation))  x (y + 10) (255,0,0) (0,0,0) imageRGB8
+          -- drawString (show $ cocoResultScore annotation)  x (y + 10) (255,0,0) (0,0,0) imageRGB8
+    case scoreThreshold of
+      Nothing -> draw
+      Just scoreThreshold -> do
+        if cocoResultScore annotation >= scoreThreshold
+          then draw
+          else return ()
+  return imageRGB8
 
 -- Show image by sixel
 showImage :: Coco -> FilePath -> FilePath -> Bool ->  IO ()
@@ -87,23 +164,28 @@ showImage coco cocoFile imageFile enableBoundingBox = do
           case imageBin' of
             Left err -> putStrLn $ "Image file " ++ imagePath ++ " can not be read."
             Right imageBin -> do
-              let imageRGB8 = convertRGB8 imageBin
-              -- Draw bounding box
-              forM_ annotations $ \annotation -> do
-                let (CoCoBoundingBox (bx,by,bw,bh)) = cocoAnnotationBbox annotation
-                    x = round bx
-                    y = round by
-                    width = round bw
-                    height = round bh
-                drawRect x y (x+width) (y+height) (255,0,0) imageRGB8
-                drawString (T.unpack (cocoCategoryName (categories !! (cocoAnnotationCategory annotation - 1)))) x y (255,0,0) (0,0,0) imageRGB8
-                return ()
-              putOSC imageRGB8
-              putStrLn ""
+              imageRGB8 <- drawBoundingBox imageBin annotations categories
+              putImage (Right imageRGB8)
     else do
-      putImage imagePath
-      putStrLn ""
+      putImage (Left imagePath)
 
+showDetectionImage :: Coco -> FilePath -> FilePath -> FilePath -> Maybe Double-> IO ()
+showDetectionImage coco cocoFile cocoResultFile imageFile scoreThreshold = do
+  let cocoFileNameWithoutExtension = takeBaseName cocoFile
+      imageDir = takeDirectory (takeDirectory cocoFile) </> cocoFileNameWithoutExtension </> "images"
+      imagePath = imageDir </> imageFile
+  cocoResult <- readCocoResult cocoResultFile
+  let image' = getCocoResultByFileName coco cocoResult imageFile
+  case image' of
+    Nothing -> putStrLn $ "Image file " ++ imageFile ++ " is not found."
+    Just (image, annotations) -> do
+      imageBin' <- readImage imagePath
+      case imageBin' of
+        Left err -> putStrLn $ "Image file " ++ imagePath ++ " can not be read."
+        Right imageBin -> do
+          let categories = cocoCategories coco
+          imageRGB8 <- drawDetectionBoundingBox imageBin (filter (\res -> cocoResultImageId res == cocoImageId image) cocoResult) categories scoreThreshold
+          putImage (Right imageRGB8)
 
 
 bashCompletion :: IO ()
@@ -118,7 +200,9 @@ opts = subparser
   ( command "list-images" (info (ListImages <$> argument str (metavar "FILE")) (progDesc "list all images of coco file"))
   <> command "list-categories" (info (ListCategories <$> argument str (metavar "FILE")) (progDesc "list all categories of coco file"))
   <> command "list-annotations" (info (ListAnnotations <$> argument str (metavar "FILE")) (progDesc "list all annotations of coco file"))
+  <> command "list-coco-result" (info (ListCocoResult <$> argument str (metavar "FILE")) (progDesc "list all coco result"))
   <> command "show-image" (info (ShowImage <$> argument str (metavar "FILE") <*> argument str (metavar "IMAGE_FILE") <*> switch (long "enable-bounding-box" <> short 'b' <> help "enable bounding box")) (progDesc "show image by sixel"))
+  <> command "show-detection-image" (info (ShowDetectionImage <$> argument str (metavar "FILE") <*> argument str (metavar "RESULT_FILE") <*> argument str (metavar "IMAGE_FILE") <*> optional (option auto (long "score-threshold" <> short 's' <> help "score threshold"))) (progDesc "show detection image by sixel"))
   <> command "bash-completion" (info (pure BashCompletion) (progDesc "bash completion"))
   )
 
@@ -132,11 +216,24 @@ main = do
   if cmd == BashCompletion
   then bashCompletion
   else do
-    coco <- readCoco (cocoFile cmd)
     case cmd of
-      ListImages _ -> listImages coco
-      ListCategories _ -> listCategories coco
-      ListAnnotations _ -> listAnnotations coco
-      ShowImage _ imageFile enableBoundingBox -> showImage coco (cocoFile cmd) imageFile enableBoundingBox
+      ListImages cocoFile -> do
+        coco <- readCoco cocoFile
+        listImages coco
+      ListCategories cocoFile -> do
+        coco <- readCoco cocoFile
+        listCategories coco
+      ListAnnotations cocoFile -> do
+        coco <- readCoco cocoFile
+        listAnnotations coco
+      ListCocoResult cocoResultFile -> do
+        cocoResult <- readCocoResult cocoResultFile
+        listCocoResult cocoResult
+      ShowImage cocoFile imageFile enableBoundingBox -> do
+        coco <- readCoco cocoFile
+        showImage coco cocoFile imageFile enableBoundingBox
+      ShowDetectionImage cocoFile cocoResultFile imageFile scoreThreshold -> do
+        coco <- readCoco cocoFile
+        showDetectionImage coco cocoFile cocoResultFile imageFile scoreThreshold
       _ -> return ()
 
--- src/Coco.hs
+++ src/Coco.hs
@@ -104,7 +104,6 @@ data CocoAnnotation = CocoAnnotation
   , cocoAnnotationSegment  :: Maybe [[Double]] -- [[x1, y1, x2, y2, ...]]
   , cocoAnnotationArea     :: Double
   , cocoAnnotationBbox     :: CoCoBoundingBox
-  , cocoAnnotationScore    :: Maybe Double
   , cocoAnnotationIsCrowd  :: Maybe Int
   } deriving (Show, Eq, Generic)
 
@@ -116,7 +115,6 @@ instance FromJSON CocoAnnotation where
     cocoAnnotationSegment  <- o .:? "segmentation"
     cocoAnnotationArea     <- o .: "area"
     cocoAnnotationBbox     <- fmap (\[x,y,w,h] -> CoCoBoundingBox (x,y,w,h)) $ o .: "bbox"
-    cocoAnnotationScore    <- o .:? "score"
     cocoAnnotationIsCrowd  <- o .:? "iscrowd"
     return CocoAnnotation{..}
 
@@ -177,6 +175,31 @@ instance ToJSON Coco where
     , "categories"  .= cocoCategories
     ]
 
+-- Coco result format is shown in https://cocodataset.org/#format-results .
+
+data CocoResult = CocoResult
+  { cocoResultImageId :: Int
+  , cocoResultCategory :: Int
+  , cocoResultScore :: Double
+  , cocoResultBbox :: CoCoBoundingBox
+  } deriving (Show, Eq, Generic)
+
+instance FromJSON CocoResult where
+  parseJSON = withObject "result" $ \o -> do
+    cocoResultImageId  <- o .: "image_id"
+    cocoResultCategory <- o .: "category_id"
+    cocoResultScore    <- o .: "score"
+    cocoResultBbox     <- fmap (\[x,y,w,h] -> CoCoBoundingBox (x,y,w,h)) $ o .: "bbox"
+    return CocoResult{..}
+
+instance ToJSON CocoResult where
+  toJSON CocoResult{..} = object
+    [ "image_id"    .= cocoResultImageId
+    , "category_id" .= cocoResultCategory
+    , "score"       .= cocoResultScore
+    , "bbox"        .= case cocoResultBbox of CoCoBoundingBox (x,y,w,h) -> [x,y,w,h]
+    ]
+
 readCoco :: FilePath -> IO Coco
 readCoco path = do
   json <- BS.readFile path
@@ -187,6 +210,16 @@ readCoco path = do
 writeCoco :: FilePath -> Coco -> IO ()
 writeCoco path coco = BS.writeFile path $ encode coco
 
+readCocoResult :: FilePath -> IO [CocoResult]
+readCocoResult path = do
+  json <- BS.readFile path
+  case eitherDecode json of
+    Left err -> error err
+    Right coco -> return coco
+
+writeCocoResult :: FilePath -> [CocoResult] -> IO ()
+writeCocoResult path coco = BS.writeFile path $ encode coco
+
 getCocoImageByFileName :: Coco -> FilePath -> Maybe (CocoImage, [CocoAnnotation])
 getCocoImageByFileName coco fileName = 
   case filter (\CocoImage{..} -> T.unpack cocoImageFileName == fileName) $ cocoImages coco of
@@ -194,3 +227,11 @@ getCocoImageByFileName coco fileName =
     (x:_) -> 
       let annotations = filter (\CocoAnnotation{..} -> cocoAnnotationImageId == cocoImageId x) $ cocoAnnotations coco
       in Just (x, annotations)
+
+getCocoResultByFileName :: Coco -> [CocoResult] -> FilePath -> Maybe (CocoImage, [CocoResult])
+getCocoResultByFileName coco cocoResult fileName = 
+  case filter (\CocoImage{..} -> T.unpack cocoImageFileName == fileName) $ cocoImages coco of
+    [] -> Nothing
+    (x:_) -> 
+      let results = filter (\CocoResult{..} -> cocoResultImageId == cocoImageId x) cocoResult
+      in Just (x, results)
